8be068d35139436d2c33a376a107d4d45597ca4d
monkeyband114 <gadocer54@gmail.com>
2025-04-29 11:46:04 +0100

    Changes to be committed: 	modified:   app.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/cors/CONTRIBUTING.md 	new file:   node_modules/cors/HISTORY.md 	new file:   node_modules/cors/LICENSE 	new file:   node_modules/cors/README.md 	new file:   node_modules/cors/lib/index.js 	new file:   node_modules/cors/package.json 	new file:   node_modules/object-assign/index.js 	new file:   node_modules/object-assign/license 	new file:   node_modules/object-assign/package.json 	new file:   node_modules/object-assign/readme.md 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
 const authRoutes = require("./routes/authRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const riderRoutes = require("./routes/riderRoutes");
 require("dotenv").config();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
